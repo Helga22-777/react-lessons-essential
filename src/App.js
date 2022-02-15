3d7a09dab2dad0b72b88dfafc06cec3742bda49d
Helga22-777 <kisa.manenka@gmail.com>
2022-02-15 16:37:01 +0200

    feat: new component for homework1.3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Homework from './components/homework';
 
 class App extends React.Component {
   render () {
     return (
       <div className="App">
         <h1>Hello World</h1>
+        <Homework />
       </div>
     );
   } 
---
+++ src/components/homework/index.js
@@ -0,0 +1,11 @@
+import panda from '../../assets/img/panda.jpg'
+const Homework = () => {
+    return (
+        <div>
+            <p>Мой первый тескт в React. Дальше добавлена картинка из папки assets</p>
+            <img src={panda} alt="panda" />
+        </div>
+    )
+}
+
+export default Homework;
